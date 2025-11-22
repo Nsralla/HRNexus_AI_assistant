2e517380305e4faa13feeb885a05d70261c8ac1d
nsralla hasan <nsrhassan22@gmail.com>
2025-11-22 11:48:00 +0200

    fixing CROS MIDDLEWARE

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,14 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:5174",
+        "http://127.0.0.1:3000",
+        "https://hr-nexus-ai-assistant-nuar.vercel.app",
+        "https://hr-nexus-ai-assistant-nuar.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
